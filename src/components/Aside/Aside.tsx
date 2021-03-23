b31f793b2044888e6c43ced9e1ccff149e929257
Jessica Apolinar <jessica_220691@hotmail.com>
2021-03-23 00:31:02 -0600

    feat: Add edit aside data. (#24)
    * feat: Add edit aside data.
    
    * feat: Add format aside component.
    
    Co-authored-by: Jessica Apolinar <jessica.uk220691@gmail.com>

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,31 +1,31 @@
 import React, { useEffect, useState, useContext } from "react";
-import ReactFlagsSelect from 'react-flags-select';
+import ReactFlagsSelect from "react-flags-select";
+import IconButton from "@material-ui/core/IconButton";
+import CreateIcon from "@material-ui/icons/Create";
 import { PROFILE, LANGUAGES } from "../../utils/constants";
-import { get } from "../../utils/Requester";
+import { get, post } from "../../utils/Requester";
 import { setLanguageCode } from "../../utils/Functions";
 import { US } from "../../utils/constants";
-import LayoutContext from '../Layout/LayoutContext';
+import LayoutContext from "../Layout/LayoutContext";
 import "./Aside.css";
 
-
-
-export interface TranslateValue{
+export interface TranslateValue {
   en: string;
   es: string;
-};
-export interface Languages{
+}
+export interface Languages {
   id: number;
   label: TranslateValue;
   percentage?: number;
-};
-export interface Translate<TranslateValue>{
-  [key:string]: TranslateValue;
-};
+}
+export interface Translate<TranslateValue> {
+  [key: string]: TranslateValue;
+}
 interface Contact {
   email: string | undefined;
   linkedin: string | undefined;
   phone: string | undefined;
-};
+}
 
 interface Profile {
   picture: string | undefined;
@@ -33,32 +33,36 @@ interface Profile {
   lastName: string | undefined;
   contact: Contact;
   aboutMe: Translate<string>;
-};
+}
 
 function Aside() {
   const ContextLayout = useContext(LayoutContext);
   const { idLanguage, setIdLanguage } = ContextLayout;
-  const [ languagesCatalog, setLanguagesCatalog ] = useState<Languages[]>([]);
+  const [languagesCatalog, setLanguagesCatalog] = useState<Languages[]>([]);
 
   const [data, setData] = useState<Profile>({
-    picture: '',
-    firstName: '',
-    lastName: '',
+    picture: "",
+    firstName: "",
+    lastName: "",
     contact: {
-      email: '',
-      linkedin: '',
-      phone: '',
+      email: "",
+      linkedin: "",
+      phone: "",
     },
     aboutMe: {
-      en: '',
-      es: '',
-    }
+      en: "",
+      es: "",
+    },
   });
   const [selected, setSelected] = useState<string>(US.toUpperCase());
 
   const getData = async () => {
     const response = await get(PROFILE);
     setData(response);
+  };
+  const postData = async (dataParam: Profile) => {
+    const dataObject = { ...dataParam };
+    await post(PROFILE, dataObject);
   };
   const getLanguagesCatalog = async () => {
     const response = await get(LANGUAGES);
@@ -69,8 +73,8 @@ function Aside() {
     getLanguagesCatalog();
   }, []);
 
-  const {email, linkedin, phone}  = data.contact;
-  const aboutMeTranslate  = data.aboutMe[idLanguage];
+  const { email, linkedin, phone } = data.contact;
+  const aboutMeTranslate = data.aboutMe[idLanguage];
 
   return (
     <aside className="aside">
@@ -86,26 +90,39 @@ function Aside() {
               src={process.env.PUBLIC_URL + "/assets/vector-line2.png"}
               alt=""
             />
-            <img src={process.env.PUBLIC_URL + "/assets/blob-2.svg"} alt="" />
+            <img src={process.env.PUBLIC_URL + "/assets/blob.svg"} alt="" />
           </div>
-          <img className="picture" src={process.env.PUBLIC_URL + "/assets/profile-picture.png"} alt="" />
+          <img
+            className="picture"
+            src={process.env.PUBLIC_URL + "/assets/profile-picture.png"}
+            alt=""
+          />
         </div>
-        <ReactFlagsSelect
-          countries={["US", "ES"]}
-          customLabels={{"US": "EN", "ES": "ES"}}
-          selected={selected}
-          onSelect={code => {
-            setIdLanguage(setLanguageCode(code.toLowerCase()));
-            setSelected(code)
-          }}
-          
-        />
+        <div className="flex-container">
+          <ReactFlagsSelect
+            countries={["US", "ES"]}
+            customLabels={{ US: "EN", ES: "ES" }}
+            selected={selected}
+            onSelect={(code) => {
+              setIdLanguage(setLanguageCode(code.toLowerCase()));
+              setSelected(code);
+            }}
+          />
+          <IconButton
+            aria-label="Edit"
+            className="btn-edit"
+            onClick={async () => {
+              console.log("click edit");
+            }}
+          >
+            <CreateIcon />
+          </IconButton>
+        </div>
+
         <div className="profile-information">
           <h1>{data.firstName}</h1>
           <h3>{data.lastName}</h3>
-          <p className="about">
-            {aboutMeTranslate}
-          </p>
+          <p className="about">{aboutMeTranslate}</p>
           <div className="profile-contact">
             <h3>Contacto</h3>
             <p>
